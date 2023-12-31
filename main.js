e48122b733c2ce441de7ebed92d0dfeca7d93859
sameer jain <92518021+Coder-sam-007@users.noreply.github.com>
2023-12-31 10:57:38 +0530

    Js beautified

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-const socket = io("http://localhost:3000");
+const socket = io("http://localhost:3000" || "https://chat-gsm9.onrender.com");
 
-document.querySelector("form").addEventListener("submit", function (e) {
+const form = document.querySelector("form");
+const m = document.getElementById("m");
+const messages = document.getElementById("messages");
+
+form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  socket.emit("chat message", document.getElementById("m").value);
-
-  document.getElementById("m").value = "";
+  socket.emit("chat message", m.value);
+  m.value = "";
 });
 
 socket.on("chat message", function (msg) {
   const li = document.createElement("li");
   li.textContent = msg;
-  document.getElementById("messages").appendChild(li);
+  messages.appendChild(li);
 });
