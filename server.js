5b0ddeecd704b9a31e94a7d4346840c16fe6ba6b
coder-sam-007 <sameerjain.coder@gmail.com>
2023-12-31 10:31:35 +0530

    error fixed

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
-
+const express = require("express");
+const http = require("http");
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
 
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
 
   // Handle chat messages
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
   });
 
   // Handle disconnection
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
   });
 });
 
